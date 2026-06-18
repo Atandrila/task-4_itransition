d012eec6fd210fa21cfc2eeb055e9a5963b7d092
Atandrila <138500994+Atandrila@users.noreply.github.com>
2026-06-18 20:43:36 +0600

    Add ForgotPassword and ResetPassword routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
 import Users from "./pages/Users";
 
 export default function App() {
@@ -10,6 +12,8 @@ export default function App() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/users" element={<Users />} />
       </Routes>
     </BrowserRouter>
