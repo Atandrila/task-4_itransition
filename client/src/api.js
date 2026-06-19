9a727fccfcb9050c1343d3dda838005f33acca94
Atandrila <138500994+Atandrila@users.noreply.github.com>
2026-06-19 15:17:39 +0000

    i think done now

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "/api",
+  baseURL: "https://task-4itransition-production.up.railway.app/api",
   withCredentials: true
 });
 
