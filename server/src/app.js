096f0ee656068bcebbd82d22961c26150f30023c
Atandrila <138500994+Atandrila@users.noreply.github.com>
2026-06-19 14:48:41 +0000

    Fix Railway listening port

--- server/src/app.js
+++ server/src/app.js
@@ -56,10 +56,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 const PORT = process.env.PORT || 5000;
 
+const PORT = process.env.PORT || 8079;
+
 if (!process.env.VERCEL) {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
 }
-
-export { app }
+export { app };
